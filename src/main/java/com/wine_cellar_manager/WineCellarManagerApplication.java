49ca1eaff3ab9e4a2832c14112f356aa46152713
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2025-04-11 17:23:16 -0300

    commit inicial

---
+++ src/main/java/com/wine_cellar_manager/WineCellarManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wine_cellar_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineCellarManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineCellarManagerApplication.class, args);
+	}
+
+}
